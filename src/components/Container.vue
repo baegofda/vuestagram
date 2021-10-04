adb4807819a9c0e3defe002e9e374dd6d6d1606c
baegofda <molly.daleo3o@gmail.com>
2021-10-04 20:12:14 +0900

    vuestagram

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post v-for="(post, idx) in posts" :key="idx" :post="post" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "Container",
+  components: {
+    Post,
+  },
+  props: {
+    posts: Array,
+  },
+};
+</script>
+
+<style></style>
